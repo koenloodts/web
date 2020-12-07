8199a3e6ab482d140dde6519f057ad50426fff39
koen <koen.loodts@dynapps.be>
2020-12-07 19:33:38 +0100

    [14.0-mig-web] [MIG] web_ir_actions_act_window_message: Migration to 14.0

--- web_ir_actions_act_window_message/__manifest__.py
+++ web_ir_actions_act_window_message/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Client side message boxes",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "Therp BV, " "ACSONE SA/NV, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Hidden/Dependency",
